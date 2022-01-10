d3e2cbc13e11596c271598e29716b26749de65b2
MichaelFellas <MichaelAriti94@gmail.com>
2022-01-10 18:35:58 +0800

    final upload, ReadME done now

--- Assets/Javascript/javascript.js
+++ Assets/Javascript/javascript.js
@@ -12,22 +12,27 @@ var answerKey =["Down","*", "HTML","append", "All of the above","Javascript", "F
 var reset = document.querySelector(".buttonTwo")
 var correct = '0'
 var timeRemaining = '';
-var clickMe = document.querySelectorAll(".button")[0]
-var clickMe2 = document.querySelectorAll(".button")[1]
-var clickMe3 = document.querySelectorAll(".button")[2]
-var clickMe4 = document.querySelectorAll(".button")[3]
+var clickMe = document.querySelectorAll(".buttonOne")[0]
+var clickMe2 = document.querySelectorAll(".buttonOne")[1]
+var clickMe3 = document.querySelectorAll(".buttonOne")[2]
+var clickMe4 = document.querySelectorAll(".buttonOne")[3]
 var h3;
-var highScores = [];
+var score = [];
+var gameStart = document.getElementById("#startGame")
+var lander = document.querySelector(".lander")
+var quiz = document.querySelector(".quiz")
+var scoreboard = document.querySelectorAll("h4")
+const scores = ""
 
-//Confirm prompt to start the game//
-if (confirm("Would you like to play the Coding Quiz?") == true) {
-    countdown();
-}
-else {
-    document.body.innerHTML = ("");
-}
+//Button that starts the game 
+gameStart.addEventListener("click", function () {
+hide(lander)
+show(quiz)
+countdown ();
+questionPicker ();
+});
 
-//loads the questions based on array text and prints them to buttons //
+//loads the questions based on array text and prints them to buttons 
 function loadQuestions () {  
    document.getElementById("#answer-display-1").innerHTML = answerA[globalThis.randomChoice];
    document.getElementById("#answer-display-2").innerHTML = answerB[globalThis.randomChoice]; 
@@ -36,7 +41,7 @@ function loadQuestions () {
    document.getElementById("questions").textContent = question[globalThis.randomChoice]; 
 }
 
-//Picks random number for questions//
+//Picks random number for questions
 function randomNumber () {
     var randomChoice = "";
     var randomChoice = questionNumber[Math.floor(Math.random ()*questionNumber.length)];
@@ -45,31 +50,34 @@ function randomNumber () {
     if (index > -1) {
         questionNumber.splice(index,1);        
     }
-    loadQuestions ();
-    // Remove these before submission//
+    loadQuestions ();    
 }
 
-//determines if there's more questions to be picked//
+//Determines if there's more questions to be picked
 function questionPicker() {
     if (questionNumber.length===0) {        
-        highscores();
-        
+        saveHighscores();        
     }
 
     else {
-        randomNumber();
-        
+        randomNumber();        
     }
-
 }
 
-questionPicker ();
+//Hide funstion
+function hide(element) {
+    element.setAttribute("style", "display:none")
+}
 
+//Show function
+function show(element) {
+    element.setAttribute("style", "display:block")
+}
 
-
-//Countdown function//
-
+//Countdown function
+//BUG On page reload Countdown will go down at twice the speed, on second reload 3 times the speed
 function countdown() {
+    
     var timeLeft = 150;
     globalThis.timeLeft = timeLeft
    
@@ -78,12 +86,13 @@ function countdown() {
       timerEl.textContent = "Time Remaining: " + globalThis.timeLeft;
   
       if(globalThis.timeLeft === 0) {      
-        highscores();
+        saveHighscores();
       }
          
     },1000);
 }
 
+//Buttons for answers
 clickMe.addEventListener("click", function () {
      
     if (answerA[globalThis.randomChoice] === answerKey[globalThis.randomChoice]) {
@@ -142,75 +151,98 @@ clickMe4.addEventListener("click", function () {
     
 });
 
-
 // Highscores Function
-function highscores () {
+function saveHighscores () {
     var timeScore = globalThis.timeLeft;
-        document.body.innerHTML = "";
+        hide(quiz);
         var initials = prompt("Please enter your initials");
-        var score = {
+        if (initials === null){
+        resetty();
+        return;
+        }
+        var result = {
             correctAnswers: correct,
             timeRemaining: timeScore,
             name: initials,
         }
-        highScores.push(score);
-        var score = "";
-    globalThis.timeLeft = -1;
-    var scoreBox = document.createElement("h3");
+        score.push(result);
+        initials = "";        
+        storage ();    
+        globalThis.timeLeft === -1;
+loadHighscores();   
+}
+
+//Loads highscores in the order they are made, was unable to sort them.
+//BUG h4's get printed on the 3rd refresh onwards and I do not know why or how.
+//BUG local storage is wiped on page refresh. unable to figure out.
+function loadHighscores() {
+    
+    const scoreBox = document.createElement("h3");
     scoreBox.textContent = "HIGHSCORES";
     document.body.appendChild(scoreBox);
-    highScores.sort(function(a, b){return b-a});
-
-    for (var i = 0; i < 7; i++) {
-    var score = document.createElement("h4");
-    score.textContent = "HIGHSCORE "+[i+1]+": "+ highScores[i].name + ", " + highScores[i].correctAnswers + " Correct Answers, " + highScores[i].timeRemaining + " seconds left." ;
-    document.body.appendChild(score);
+    globalThis.scoreBox = scoreBox
+    loadScores(); 
+    for (var i = 0; i < score.length; i++) {
+        if (i< 6){          
+        const scores = document.createElement("h4");
+        globalThis.scores = scores;     
+        globalThis.scores.textContent = "HIGHSCORE "+[i+1]+": "+ score[i].name + ", " + score[i].correctAnswers + " Correct Answers, " + score[i].timeRemaining + " seconds left." ;
+        document.body.appendChild(globalThis.scores);
+        
     }
+    }
+    replay();
+    return;
+}
 
-    console.log(highScores);
 
+//Starts the game again via replay button
+function replay () {
     var reset2 = document.createElement("Button");
     reset2.textContent = "REPLAY GAME"
     document.body.appendChild(reset2);
     reset2.setAttribute("style", "display: block; margin-right:auto; margin-left:auto; background-color: inherit; border-radius: 5px; color: white; cursor: pointer; font-size: 35px; padding: 10px;");
-    reset2.addEventListener('click', function () {
-        document.location.reload();
+    reset2.addEventListener('click', function () {  
+        globalThis.scores.setAttribute("style", "display: none")
+        globalThis.scores.remove();    
+        globalThis.scoreBox.remove();
+        reset2.remove();        
+        var questionNumber = [0,1,2,3,4,5,6,7,8,9];
+        globalThis.questionNumber = questionNumber
+        show(lander); 
+        globalThis.timeLeft === -1;       
       });
 }
 
+//Stores Scores to local storage
+function storage (){
+    localStorage.setItem("scores", JSON.stringify(score))
+}
 
+//Loads scores from local storage
+function loadScores (){
+    const storageScores = JSON.parse(localStorage.getItem("scores"))
+    if (storageScores.length > 0) {
+        score = storageScores
+    }
+}
 
+//Resets the game
+function resetty () {
+    if (globalThis.scores=0){
+        globalThis.scores.remove();  
+    }     
+    if (globalThis.scoreBox=0){ 
+        globalThis.scoreBox.remove();   
+    } 
+        var questionNumber = [0,1,2,3,4,5,6,7,8,9];
+        globalThis.questionNumber = questionNumber
+        show(lander);
+        hide(quiz);
+        globalThis.timeLeft = -1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 // Reset Button//
-reset.addEventListener('click', function () {
-    document.location.reload();
-  });
+reset.addEventListener('click', resetty);
+
---
+++ ReadMe.md
@@ -0,0 +1,40 @@
+# Homework Week 4 Coding Quiz Michael Ariti
+
+## Coding Quiz
+
+The purpose of this repository is to create a small multiple choice javascript game based on some questions about javascript.
+
+## Features
+
+1. 10 Questions that are picked randomly and do not repeat.
+2. A Timer that counts down from 150 seconds.
+3. The ability to save your highscore.
+4. Responsive buttons, including a replay and a reset button.
+
+## File Directory
+
+You will find the index.html next to this readme, which is the finished website code.
+
+In the assets folder you will find 
+1. The guidelines to this homework
+2. The css style sheet for the index.html
+3. The screenshot of the finished website which is pictured below.
+4. The javascript used to run the generator.
+
+## Bugs
+There are several bugs unfortunately that I have found listed below
+1. On page reload Countdown will go down at twice the speed, on second reload 3 times the speed.
+2. h4's get printed on the 3rd refresh onwards.
+3. local storage is wiped on page refresh. 
+
+## Secreenshot
+
+![Coding-Quiz-Screenshot](./assets/images/Screenshot.png)
+
+## Webpage URL
+[Coding-Quiz-URL](https://michaelfellas.github.io/Coding-Quiz/)
+
+
+### End
+
+Michael Ariti 2021
